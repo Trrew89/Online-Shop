80019f9b43d091ad35fd644dc74405806fb227ed
Sveatoslav <sveatoslavbodrus@gmail.com>
2023-06-13 13:20:58 +0200

    added error and ErrorHandler

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const sequelize = require('./db.js')
 const cors = require('cors');
 const models = require('./models/models.js');
 const router = require('./routes/index.js');
-
+const errorHandler = require('./middleware/ErrorHandlingMiddleware.js')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
-
+app.use(errorHandler);
 
 const start = async () => {
     try {
