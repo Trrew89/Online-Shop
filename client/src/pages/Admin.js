3b0282c69165008c39d1f20b1468d356cd25f8c6
Sveatoslav <sveatoslavbodrus@gmail.com>
2023-06-14 18:53:30 +0200

    created navigation for the webseite

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => {
+    console.log(1);
+    return (
+        <div>
+           Admin 
+        </div>
+    );
+};
+
+export default Admin;
---
+++ client/src/pages/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {
+    console.log(1);
+    return (
+        <div>
+            SHOP
+        </div>
+    );
+};
+
+export default Shop;
