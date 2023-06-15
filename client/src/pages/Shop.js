9301e456605d23af48e923b02b2f05735409e2f4
Sveatoslav <sveatoslavbodrus@gmail.com>
2023-06-15 14:05:17 +0200

    created brand and device bars, also added list of devices (first variation)

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import TypeBar from '../components/TypeBar';
+import BrandBar from '../components/BrandBar';
+import DeviceList from '../components/DeviceList';
 
 const Shop = () => {
-    console.log(1);
     return (
-        <div>
-            SHOP
-        </div>
+        <Container>
+            <Row className='mt-3'>
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+                    <BrandBar/>
+                    <DeviceList/>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -3,19 +3,34 @@ import {makeAutoObservable} from 'mobx'
 export default class DeviceStore {
     constructor() {
         this._types = [
-            {id: 1, name: 'Refrigerator'},
-            {id: 2, name: 'Smartphones'}
+            {id: 1, name: 'Refrigerators'},
+            {id: 2, name: 'Smartphones'},
+            {id: 3, name: 'Laptops'},
+            {id: 4, name: 'TV'}
         ]
         this._brands = [
             {id: 1, name: 'Samsung'},
-            {id:2, name:'Appel'}
+            {id: 2, name:'Apple'},
+            {id: 3, name: 'Lenovo'},
+            {id: 4, name:'Xiaomi'},
         ]
         this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
             {id: 2, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
             {id: 3, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
             {id: 4, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 5, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 6, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 7, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 8, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 9, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 10, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 11, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+            {id: 12, name: "Iphone 12 pro", price: 900, rating: 5, img: `https://d10cggh4v5dmy2.cloudfront.net/media/catalog/product/cache/17faa7f491c70ff08effdc50b0fd4c4b/i/p/iphone_12_pro_max_silver_full_cover_7db0.jpg`},
+        
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -29,15 +44,26 @@ export default class DeviceStore {
     setDevices(devices) {
         this._devices = devices
     }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
 
     get types() {
         return this._types
     }
-
     get brands() {
         return this._brands
     }
     get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
+    }
+    get selectedBrand() {
+        return this._selectedBrand
     }
 }
